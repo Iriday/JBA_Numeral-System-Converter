ea2d7da2645af6eb02d070d0be78c4ce5a72a5a0
Igor <GI_Something@protonmail.com>
2020-11-03 19:09:41 +0200

    feat: Impl program that can convert positive integer in base 10 to base 2-10

--- src/main/kotlin/converter/main.kt
+++ src/main/kotlin/converter/main.kt
@@ -1,3 +1,26 @@
 package converter
 
-fun main() = print("17 is equal to 0b10001: ${17 == 0b10001}")
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.math.RoundingMode
+
+fun main() {
+    print(decimalTo(readNum(), 2))
+}
+
+fun decimalTo(decimal: BigInteger, base: Int): String {
+    val b = base.toBigDecimal()
+    val sb = StringBuilder()
+    var left = decimal.toBigDecimal()
+
+    while (true) {
+        val numStr = (left.divide(b,10, RoundingMode.UP)).toPlainString()
+        left = numStr.substringBefore('.').toBigDecimal()
+        val right = numStr.substring(numStr.indexOf('.')).toBigDecimal() * b
+        sb.append(right.toInt())
+
+        if (left == BigDecimal.ZERO) return sb.reverse().toString()
+    }
+}
+
+fun readNum() = readLine()!!.toBigInteger()
