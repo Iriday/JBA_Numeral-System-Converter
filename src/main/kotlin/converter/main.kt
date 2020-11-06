61566ab4e5a8c643a760d138eb0cf99f2ac265d9
Igor <GI_Something@protonmail.com>
2020-11-06 09:27:12 +0200

    feat: Add possibility to convert positive integer in base 2-36 to base 2-36

--- src/main/kotlin/converter/main.kt
+++ src/main/kotlin/converter/main.kt
@@ -3,12 +3,18 @@ package converter
 import java.math.BigDecimal
 import java.math.BigInteger
 import java.math.RoundingMode
+import kotlin.math.pow
 
 fun main() {
+    run()
+}
+
+fun run() {
     val prefixes = mapOf(Pair(2, "0b"), Pair(8, "0"), Pair(16, "0x"))
-    val num = readNum()
-    val base = readNum().toInt()
-    print(addPrefix(decimalTo(num, base), base, prefixes))
+    val srcBase = readNum()
+    val num = readStr()
+    val trgBase = readNum()
+    print(addPrefix(decimalTo(toDecimal(num, srcBase), trgBase), trgBase, prefixes))
 }
 
 fun decimalTo(decimal: BigInteger, base: Int): String {
@@ -26,8 +32,20 @@ fun decimalTo(decimal: BigInteger, base: Int): String {
     }
 }
 
+fun toDecimal(number: String, base: Int): BigInteger {
+    val baseDouble = base.toDouble()
+    var decimal = BigDecimal.ZERO
+
+    for ((i, num) in number.withIndex()) {
+        decimal += (baseDouble.pow(number.length - 1 - i)
+                * (num.toString().toIntOrNull() ?: num - 'a' + 10)).toBigDecimal()
+    }
+    return decimal.toBigInteger()
+}
+
 fun addPrefix(num: String, base: Int, prefixes: Map<Int, String>, defaultPrefix: String = ""): String {
     return prefixes.getOrDefault(base, defaultPrefix) + num
 }
 
-fun readNum() = readLine()!!.toBigInteger()
+fun readNum() = readLine()!!.toInt()
+fun readStr() = readLine()!!
